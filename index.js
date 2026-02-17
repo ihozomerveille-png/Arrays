77f7a711664673cbd6be54819afdfd3f3f6345cf
lenovo <ihozomerveille@gmail.com>
2026-02-17 15:49:12 +0200

    exercise 3

--- index.js
+++ index.js
@@ -27,3 +27,22 @@ console.log(inventory);
 // e
 inventory.splice(1, 1, 25, 35);
 console.log(inventory);
+// excercise 3 : shallow copying of Arrays and the reference trap
+//a
+const originalScores = [90, 85, 78];
+//b
+const referenceCopy = originalScores;
+console.log(referenceCopy);
+//c
+// Modifying referenceCopy will also modify originalScores because they point to the same array in memory
+referenceCopy[0] = 100;
+console.log("originalScores:", originalScores);
+console.log("referenceCopy:", referenceCopy);
+//d
+// To create a true copy of the array, we can use the spread operator or the slice method. This way, we can modify the new array without affecting the original one.
+const spreadCopy = [...originalScores]; // creates a new array with the same elements
+//e
+spreadCopy[0] = 50;
+console.log("originalScores:", originalScores);
+console.log("spreadCopy:", spreadCopy);
+
